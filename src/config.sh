1ee50189512b2b217d7fa3b84dcb3d312cdd85f9
HrishabhMittal <hrishabhmittal.hm@gmail.com>
2025-07-19 20:15:58 +0530

    simplify

--- src/config.sh
+++ src/config.sh
@@ -7,17 +7,17 @@ source print.sh
 LOGO_FILE="logo.txt"
 LOGO_COLOR="$BLUE"
 stats="\
-${BLUE}$(get_user)${RESET}${WHITE}@${RESET}${RED}$(get_hostname)${RESET}
+${BLUE}$(get_user)${WHITE}@${RED}$(get_hostname)${RESET}
 
-${GREEN}PC:${RESET} ${CYAN}$(get_model)${RESET} ${PURPLE}$(get_version)${RESET}
-${YELLOW}CPU:${RESET} ${CYAN}$(get_cpu_model)${RESET}
+${GREEN}PC: ${CYAN}$(get_model)${RESET} ${PURPLE}$(get_version)${RESET}
+${YELLOW}CPU: ${CYAN}$(get_cpu_model)${RESET}
 $(get_gpu_info | sed "s/\:/\:\\${RESET}\\${BLUE}/" | sed "s/^/\\${PURPLE}/; s/$/\\${RESET}/")
 
-${GREEN}Distro:${RESET} ${WHITE}$(get_os_name)${RESET}
-${CYAN}Kernel:${RESET} ${YELLOW}$(get_kernel_version)${RESET}
-${PURPLE}Shell:${RESET} ${GREEN}$(get_shell)${RESET}
+${GREEN}Distro: ${WHITE}$(get_os_name)${RESET}
+${CYAN}Kernel: ${YELLOW}$(get_kernel_version)${RESET}
+${PURPLE}Shell: ${GREEN}$(get_shell)${RESET}
 
-${RED}Uptime:${RESET} ${CYAN}$(get_uptime)${RESET}
+${RED}Uptime: ${CYAN}$(get_uptime)${RESET}
 "
 
 ###########################################
